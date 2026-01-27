3f977bdd54a2614931136e37ed62978d3421cdac
S0FTS0RR0W <chrlsbrwn4gt@gmail.com>
2026-01-27 16:20:10 +0000

    Fix: dev page

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -2,7 +2,7 @@ export default function Dev() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-            
+                
             </div>
         </main>
     )
