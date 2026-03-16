4a4652baddc73a37bf7e4b75ec8e6b7eddf1cc9b
S0FTS0RR0W <charlie@s0fts0rr0w.com>
2026-03-16 21:06:32 +0000

    Fix: remove unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { getRandomValues } from "crypto";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 
